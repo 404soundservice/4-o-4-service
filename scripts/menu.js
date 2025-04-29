cbd14ebf73f798590546a6ff6ba44ec51e8c3c98
404soundservice <4o4.soundservice@gmail.com>
2025-04-29 10:00:35 +0200

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+  const menu = document.getElementById('menu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
 }
