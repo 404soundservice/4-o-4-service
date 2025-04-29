05e421cf4d72d730a119bf884d72fe0f6300c09e
404soundservice <4o4.soundservice@gmail.com>
2025-04-29 20:14:57 +0200

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navMenu = document.getElementById("nav-menu");
+  const navLinks = navMenu.querySelectorAll("a");
 
   hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+  });
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      navMenu.classList.remove("active");
+    });
   });
 });
